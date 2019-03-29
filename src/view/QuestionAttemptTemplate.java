44937afc98527bf9616243728c77020453814052
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-03-29 00:27:03 -0700

    US#03-TASK#12: Added placeholder file for QuestionAttemptTemplate

---
+++ src/view/QuestionAttemptTemplate.java
@@ -0,0 +1,15 @@
+package src.view;
+
+import javax.swing.JFrame;
+
+/**
+ * Form to show a question and navaigation buttion and to give user
+ * functionality to select one of the options.
+ * @author      Sarthak Tiwari <sarthak.tiwari@asu.edu>
+ * @version     1.0
+ */
+public class QuestionAttemptTemplate extends JFrame {
+
+    //TODO: Will be implemented
+
+}
