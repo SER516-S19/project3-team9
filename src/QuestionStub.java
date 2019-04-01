843c05f4d74daf4c55ed544f7af01e4b154439a1
LAZZZZZZZy <43282464+LAZZZZZZZy@users.noreply.github.com>
2019-03-31 23:34:37 -0600

    Update QuestionStub.java

--- src/QuestionStub.java
+++ src/QuestionStub.java
@@ -1,6 +1,11 @@
 public class QuestionStub {
 
     private String description = "Some question title that defines this question and some other blah blah blah text....\n It can be multiline so this is a new line\n And this is another new line";
+    public String option1 = "First Option for this question which is too long... it is still going on... and still you are reading it.... because it is not wrapped\nOh more text is here....";
+    public String option2 = "Short Option";
+    public String option3 = "3";
+    public String option4 = "Blah Blah Blah Blah Blah Blah";
+    public short correctOption = 2;
 
     
     public String getDescription() {
