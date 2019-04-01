56bdb6074a594aca690eaea990ca3bd6993e6bd1
LAZZZZZZZy <43282464+LAZZZZZZZy@users.noreply.github.com>
2019-03-31 19:12:16 -0600

    Create TestRunner.java

---
+++ src/TestRunner.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+
+/**
+ * Form to show a question and navaigation buttion and to give user
+ * functionality to select one of the options.
+ * @author      Sarthak Tiwari <sarthak.tiwari@asu.edu>
+ * @version     1.0
+ */
+public class TestRunner {
+
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+
+        f.setSize(500, 500);
+        f.setLayout(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        QuizReader reader = new QuizReader();
+        QuizList list = new QuizList();
+
+        QuestionAttemptTemplate q = new QuestionAttemptTemplate(f);
+        QuizChecker checker = new QuizChecker(reader,list,q);
+        if(q.showQuestion(new QuestionStub())){
+            System.out.println("Correct Answer !");
+        }
+        else
+            System.out.println("Wrong Answer !");
+    }
+
+}
