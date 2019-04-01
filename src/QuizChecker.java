3c72d59dd1838049fb65a760013d684a75cf8d58
LAZZZZZZZy <43282464+LAZZZZZZZy@users.noreply.github.com>
2019-03-31 23:34:40 -0600

    Update QuizChecker.java

--- src/QuizChecker.java
+++ src/QuizChecker.java
@@ -1,25 +1,45 @@
 
 import java.util.ArrayList;
 import java.util.Random;
+/**
+ * students can try questions until they answered all questions, and if their answer is wrong, they retry this question.
+ * @author      Jiayan Wang
+ * @version     1.0
+ */
 
 public class QuizChecker {
 	private Quiz quiz;
 	private ArrayList<QuestionStub> questions;
 	private QuestionAttemptTemplate template;
+	private boolean giveup;
 	
-	public QuizChecker(QuizReader reader,QuizList list,QuestionAttemptTemplate template){
-		String quiz_title = list.getSelectedQuiz();
+//	public QuizChecker(QuizReader reader,QuizList list,QuestionAttemptTemplate template){
+//		String quiz_title = list.getSelectedQuiz();
+//		this.template = template;
+//		quiz = reader.readQuiz(quiz_title);
+//		questions = quiz.getQuestions();
+//		template.showQuestion(questions.get(0));
+//	}
+	
+	public QuizChecker(QuestionAttemptTemplate template,ArrayList<QuestionStub> q ){
+		questions = q;
 		this.template = template;
-		quiz = reader.readQuiz(quiz_title);
-		questions = quiz.getQuestions();
-		template.showQuestion(questions.get(0));
+		template.showQuestion(new QuestionStub());
+		giveup = false;
 	}
 	
-	public void retryQuestions(Question q){
-		if(!template.showQuestions(q)) {
-			Random r = new Random();
+	
+	public void tryQuestions(){
+		Random r = new Random();
+		while(questions.size()!=0 && giveup != true) {
 			int randomIndex = r.nextInt(questions.size());
-			template.showQuestion(questions.get(randomIndex));
-		}
+			while(!template.showQuestion(questions.get(randomIndex)));
+			System.out.println("1");
+			questions.remove(randomIndex);
+		}	
 	}
+
+	public boolean isGiveup() {
+		return giveup;
+	}	
 }
