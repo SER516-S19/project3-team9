be543b2bedd38e41966e80b470e0feab06238604
LAZZZZZZZy <43282464+LAZZZZZZZy@users.noreply.github.com>
2019-04-02 15:44:33 -0700

    Create Quiz.java

---
+++ model/Quiz.java
@@ -0,0 +1,28 @@
+package model;
+import java.util.ArrayList;
+
+public class Quiz {
+	private String title;
+	private ArrayList<Question> questions;
+	
+	public Quiz(String title, ArrayList<Question> questions) {
+		this.title = title;
+		this.questions = questions;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+}
