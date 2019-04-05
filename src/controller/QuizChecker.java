5d75afc39cab5ca386d4a599e980009cf3155e36
LAZZZZZZZy <43282464+LAZZZZZZZy@users.noreply.github.com>
2019-04-04 23:05:22 -0600

    Update QuizChecker.java

--- src/controller/QuizChecker.java
+++ src/controller/QuizChecker.java
@@ -8,8 +8,11 @@ import java.util.concurrent.LinkedBlockingQueue;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import src.main.SelectQuizTitle;
 import src.model.Question;
+import src.model.Quiz;
 import src.view.QuestionAttemptTemplate;
+import src.view.SelectQuizTitle;;
 /**
  * students can try questions until they answered all questions, 
  * and if their answer is wrong, they can retry this question.
@@ -18,7 +21,9 @@ import src.view.QuestionAttemptTemplate;
  */
 
 public class QuizChecker {
+	private ArrayList<Quiz> quizList;
 	private LinkedBlockingQueue<Question> questions;
+	SelectQuizTitle quiz_title;
 	private JFrame frame;
 	
 	public QuizChecker(ArrayList<Question> questionList,JFrame frame){
@@ -53,5 +58,31 @@ public class QuizChecker {
 					 System.out.println("giveup");
 				 }
 		}
+	}
+	
+	/**
+	 * This method get the quiz title from the QuizTitle class,
+	 * and then get the quiz object which has same title in the quiz list.
+	 * Based on this quiz object, it will initialize the questions queue
+	 * and send this questions queue to the Question template.
+	 * 
+	 * @param title it will be sent by the QuizTitle class
+	 */
+	public void selectTitle(String title) {
+		System.out.println("get selected title " + title);
+		for(Quiz quiz : quizList) {
+			if(quiz.getTitle().equals(title)) {
+				ArrayList<Question> questionList = quiz.getQuestions();
+				for(Question q : questionList) {
+					questions.add(q);
+				}
+				break;
+			}
+		}
+		sendQuestions();
+	}
+	
+	public void run() {
+		quiz_title = new SelectQuizTitle(frame,this);
 	}
 }
