5cc37a734eebe99aef8a6ab05a614a343f7ec5fe
Ruby Zhao <rubyqzhao@gmail.com>
2019-04-05 02:01:07 -0700

    Added QuizReader, QuizList, and test quizzes for JSON reading functionality (US#7)

--- src/main/TestRunner.java
+++ src/main/TestRunner.java
@@ -1,10 +1,15 @@
 package src.main;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
 import src.controller.QuizChecker;
 import src.model.Question;
+import src.utility.QuizReader;
+import src.view.QuizList;
+import src.model.Quiz;
 
 /**
  * Form to show a question and navaigation buttion and to give user
@@ -23,12 +28,22 @@ public class TestRunner {
         f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
-        
-        ArrayList<Question> a = new ArrayList<Question>();
-        a.add(new Question("q1","1","2","3","4",(short)(2)));
-        a.add(new Question("q2","b","bla","blabla","blablabla",(short)(1)));
-        a.add(new Question("q3","this","is","test","case",(short)(4)));
-        QuizChecker q = new QuizChecker(a,f);
-        q.sendQuestions();
+
+        QuizReader qr = new QuizReader();
+        QuizList qlist = new QuizList();
+        ArrayList<String> quizzes = qlist.readQuizList();
+        System.out.println(quizzes);
+
+        try {
+            Quiz qz = qr.readQuiz(quizzes.get(0));
+
+            ArrayList<Question> ques = qz.getQuestions();
+            QuizChecker q = new QuizChecker(ques,f);
+            q.sendQuestions();
+
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/utility/QuizReader.java
+++ src/utility/QuizReader.java
@@ -1,5 +1,92 @@
 package src.utility;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileReader;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import src.model.Quiz;
+import src.model.Question;
 
+/**
+ * Class that reads information from quiz JSON files saved in the quiz folder and stores
+ * information into a Quiz object.
+ *
+ * @author Qianru (Ruby) Zhao
+ * @verson 1.0
+ */
 public class QuizReader {
+    private final String FOLDER_PATH = "./quizzes/";
 
+    /**
+     * Constructor for the QuizReader class
+     */
+    public QuizReader() { }
+
+    /**
+     * Reads question title, options, and correct answer from the specified quiz's
+     * JSON file. Takes a quiz name and creates a JsonReader object to pass to createQuiz.
+     * @param quizName String representing the name of the quiz file (ie "testQuiz", no ".json")
+     * @throws IOException
+     */
+    public Quiz readQuiz(String quizName) throws IOException {
+        String filename = FOLDER_PATH + quizName + ".json";
+        File folder = new File(filename);
+        System.out.println(folder.getAbsolutePath());
+        JsonReader reader = new JsonReader(new FileReader(filename));
+
+        return createQuiz(reader, quizName);
+    }
+
+    /**
+     * Takes a JsonReader object and parses out the JSON file. Stores information
+     * read from file into a Quiz object.
+     * @param reader JsonReader object specifying the quiz to read from
+     * @param quizName String name of quiz file
+     * @return Quiz object requested by readQuiz
+     * @throws IOException
+     */
+    private Quiz createQuiz(JsonReader reader, String quizName) throws IOException {
+        ArrayList<Question> questionList = new ArrayList<>();
+
+        reader.beginObject();
+        System.out.println(reader.nextName()); //questions label
+        reader.beginArray();
+
+        //loop through question array
+        while(reader.hasNext() && reader.peek()!= JsonToken.END_ARRAY) {
+            Question question = new Question();
+            String correctAnswer, opt1, opt2, opt3, opt4;
+
+            reader.beginObject();
+            reader.nextName(); //title label
+            question.setDescription(reader.nextString()); //title String
+            reader.nextName(); //options label
+            reader.beginArray();
+            opt1 = reader.nextString(); //option1
+            question.setOption1(opt1);
+            opt2 = reader.nextString(); //option2
+            question.setOption2(opt2);
+            opt3 = reader.nextString(); //option3
+            question.setOption3(opt3);
+            opt4 = reader.nextString(); //option4
+            question.setOption4(opt4);
+            reader.endArray();
+            reader.nextName(); //correctAnswer label
+            correctAnswer = reader.nextString(); //correct answer option
+            reader.endObject();
+
+            //determine the correct option index
+            if(correctAnswer.equals(opt1)) question.setCorrectOption((short)1);
+            else if(correctAnswer.equals(opt2)) question.setCorrectOption((short)2);
+            else if(correctAnswer.equals(opt3)) question.setCorrectOption((short)3);
+            else if(correctAnswer.equals(opt4)) question.setCorrectOption((short)4);
+            else question.setCorrectOption((short)0);
+
+            questionList.add(question);
+        }
+        reader.endArray();
+
+        return new Quiz(quizName, questionList);
+    }
 }
